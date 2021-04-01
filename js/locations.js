e2ffa1bc7034bf8e4bbdcc8bd755567e0a83e73c
NickMol99 <78796508+NickMol99@users.noreply.github.com>
2021-04-01 20:45:47 +0200

    Add files via upload

---
+++ js/locations.js
@@ -0,0 +1,34 @@
+myLocationsList = [{
+    'type': 'Feature',
+    'properties': {
+      'description': '<strong>Landingsbaan Amsterdam</strong><p>De mooiste privé landingsbaan van Amsterdam.</p><img src="/images/landingstrip1.jpg" style="width:100%"><strong>Top 10 horecagelegenheden</strong><p>t Hooischip</p><p>t Keteltje</p><p>t Monumentje</p><p>t Sluisje</p><p>t Spuitje</p><p>Alto</p><p>Bagels and Beans</p><p>Charles 1</p><p>De Bult</p><p>De Spijker</p>',
+      'icon': 'theatre'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [4.895168, 52.370216]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'description': '<strong>Landingsbaan Utrecht</strong><p>De mooiste drukbezochte landingsbaan van Utrecht.</p><img src="/images/landingstrip3.jpg" style="width:100%"><strong>Top 10 horecagelegenheden</strong><p>Rabarber</p><p>Broodje Mario</p><p>Molencafe de Ster</p><p>Cafe Orloff</p><p>Graaf Floris</p><p>LandGoedKeuken</p><p>Teds</p><p>KEEK</p><p>Koffie en Ik</p><p>Cornelis</p>',
+      'icon': 'theatre'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [5.1214201, 52.0907374]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'description': '<strong>Landingsbaan Leiden</strong><p>De mooiste privé landingsbaan van Leiden.</p><img src="/images/landingstrip2.jpg" style="width:100%"><strong>Top 10 horecagelegenheden</strong><p>Kleine planeet</p><p>BFF Leiden</p><p>t Gerecht</p><p>t Kraantje</p><p>A-one </p><p>Aan de Rijn</p><p>Abiga Coffee</p><p>Anne en Max</p><p>Annies</p><p>Asian Palace</p>',
+      'icon': 'bar'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [4.49306, 52.15833]
+    }
+  },
+];
---
+++ js/main.js
@@ -0,0 +1,115 @@
+// API Mapbox
+mapboxgl.accessToken = 'pk.eyJ1Ijoibmlja3Jtb2wiLCJhIjoiY2tta2ppYmJ5MTFtMjJ4bXFuOHg3eGE1MSJ9.39pLiqnYMxhSXonIFxvlrg';
+
+// API openWeatherMap
+var openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/weather';
+var openWeatherMapUrlApiKey = '6b3adeb78c820e4025b76b982a28f56b';
+
+// Map
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/outdoors-v11',
+    center: [5.508852, 52.142480],
+    zoom: 7
+  });
+
+// Steden
+var steden = [
+    {
+      name: 'Amsterdam',
+      coordinates: [4.895168, 52.370216]
+    },
+    {
+      name: 'Utrecht',
+      coordinates: [5.1214201, 52.0907374]
+    },
+    {
+      name: 'Leiden',
+      coordinates: [4.49306, 52.15833]
+    },
+  ];
+
+map.on('load', function () {
+    steden.forEach(function(city) {
+      var request = openWeatherMapUrl + '?' + 'appid=' + openWeatherMapUrlApiKey + '&lon=' + city.coordinates[0] + '&lat=' + city.coordinates[1];
+      
+      fetch(request)
+        .then(function(response) {
+          if(!response.ok) throw Error(response.statusText);
+          return response.json();
+        })
+        .then(function(response) {
+          plotImageOnMap(response.weather[0].icon, city)
+        })
+        .catch(function (error) {
+          console.log('ERROR:', error);
+        });
+    });
+  });
+
+function plotImageOnMap(icon, city) {
+  map.loadImage(
+    'http://openweathermap.org/img/w/' + icon + '.png',
+    function (error, image) {
+      if (error) throw error;
+      map.addImage("weatherIcon_" + city.name, image);
+      map.addSource("point_" + city.name, {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [{
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: city.coordinates
+            }
+          }]
+        }
+      });
+      map.addLayer({
+        id: "points_" + city.name,
+        type: "symbol",
+        source: "point_" + city.name,
+        layout: {
+          "icon-image": "weatherIcon_" + city.name,
+          "icon-size": 1.3
+        }
+      });
+    }
+  );
+}
+
+// Pop up
+map.on('load', function () {
+    map.addSource('places', {
+      'type': 'geojson',
+      'data': {
+        'type': 'FeatureCollection',
+        'features': myLocationsList
+      }
+    });
+
+    map.addLayer({
+      'id': 'places',
+      'type': 'symbol',
+      'source': 'places',
+      'layout': {
+        'icon-image': '{icon}-15',
+        'icon-allow-overlap': true
+      }
+    });
+    var popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false
+    });
+    map.on('mouseenter', 'places', function (e) {
+      var coordinates = e.features[0].geometry.coordinates.slice();
+      var description = e.features[0].properties.description;
+    popup.setLngLat(coordinates)
+           .setHTML(description)
+           .addTo(map);
+    });
+    map.on('mouseleave', 'places', function () {
+      popup.remove();
+    });
+  });
